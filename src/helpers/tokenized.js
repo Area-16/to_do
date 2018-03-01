0e97511e64ef07a74e39860f7c664e54d6212368
Douglas Alves <douglas.expalves@gmail.com>
2018-03-01 15:46:32 -0300

    add resetToken function to validate the reset password token

--- src/helpers/tokenized.js
+++ src/helpers/tokenized.js
@@ -1,11 +1,11 @@
 export function resetToken ( clientToken, dbToken, expiration ) {
-  if (Date() > expiration) {
-    return { message: 'Your token timer has expired, generate a new one', status: 400, code: 'PASS_TOKEN_EXPIRED' }
-  }
+	if (Date() > expiration) {
+		return { message: 'Your token timer has expired, generate a new one', status: 400, code: 'PASS_TOKEN_EXPIRED' }
+	}
 
-  if ( clientToken !== dbToken) {
-    return { message: 'Invalid token provided', status: 400, code: 'INVALID_PASS_TOKEN' }
-  }
+	if ( clientToken !== dbToken) {
+		return { message: 'Invalid token provided', status: 400, code: 'INVALID_PASS_TOKEN' }
+	}
 
-  return true
+	return true
 }
